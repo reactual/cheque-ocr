f846090fb1159501eafb789bafecd410408d5806
Peter Graham <peterghm@gmail.com>
2016-11-16 09:09:15 -0500

    extract common testing functionality

--- test/helpers.js
+++ test/helpers.js
@@ -1,2 +1,10 @@
-global.chequeOcr = require('../index');
-global.expect = require('chai').expect;
+var fs = require('fs'),
+    chai = require('chai'),
+    chequeOCR = require('../index');
+
+global.chequeOCR = chequeOCR;
+global.expect = chai.expect;
+
+global.readFixture = function(filename) {
+  return fs.readFileSync('./test/fixtures/' + filename);
+};
--- test/ocr.js
+++ test/ocr.js
@@ -1,11 +1,10 @@
 require('./helpers');
-var fs = require('fs');
 
 describe('ocr', function() {
   describe('integration test with a valid cheque image', function() {
     it('returns a result', function(done) {
-      var image = fs.readFileSync('./test/fixtures/sample_cibc.jpg');
-      chequeOcr(image, function(err, result) {
+      var image = readFixture('sample_cibc.jpg');
+      chequeOCR(image, function(err, result) {
         expect(err).not.to.be.ok;
         expect(result.confidence).to.be.at.least(0);
         expect(result.confidence).to.be.at.most(100);
@@ -21,8 +20,8 @@ describe('ocr', function() {
 
   describe('integration test with a partial cheque image', function() {
     it('returns the expected error', function(done) {
-      var image = fs.readFileSync('./test/fixtures/invalid_partial_numbers.jpg');
-      chequeOcr(image, function(err, result) {
+      var image = readFixture('invalid_partial_numbers.jpg');
+      chequeOCR(image, function(err, result) {
         expect(err).to.eql({error: 'NO_CHEQUE_NUMBERS_FOUND'});
         expect(result.numbers).not.to.be.ok;
         done();
@@ -32,8 +31,8 @@ describe('ocr', function() {
 
   describe('integration test with a non-cheque image that contains no text', function() {
     it('returns the expected error', function(done) {
-      var image = fs.readFileSync('./test/fixtures/invalid_no_text.png');
-      chequeOcr(image, function(err, result) {
+      var image = readFixture('invalid_no_text.png');
+      chequeOCR(image, function(err, result) {
         expect(err).to.eql({error: 'NO_TEXT_BLOCKS_FOUND'});
         expect(result.numbers).not.to.be.ok;
         done();
